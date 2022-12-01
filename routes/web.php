1c9aaf58e5a692807d0e617987a16dd0336720ca
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2022-12-01 20:22:58 +0100

    Poprawka web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KontrolerStart;
 use App\Http\Controllers\PostController;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
